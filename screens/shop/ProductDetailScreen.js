4548120df840bbb52c08ec42635897200c3885a5
marcosdellavecchia <marcosdvecchia@gmail.com>
2021-03-01 10:47:54 -0300

    Added project structure and product card styling

---
+++ screens/shop/ProductDetailScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Button,
+  ScrollView,
+} from "react-native";
+
+const ProductDetailScreen = (props) => {
+  return <View></View>;
+};
+
+export default ProductDetailScreen;
